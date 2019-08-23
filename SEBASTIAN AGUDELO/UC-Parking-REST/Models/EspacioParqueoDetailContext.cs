d7dfc6c4153a65feb4878300bcf325b7878acf9f
sebastian <juan.1701514210@ucaldas.edu.co>
2019-08-23 03:55:10 -0500

    agregando detail context

---
+++ SEBASTIAN AGUDELO/UC-Parking-REST/Models/EspacioParqueoDetailContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UC_Parking_REST.Models
+{
+    public class EspacioParqueoDetailContext : DbContext
+    {
+
+        public EspacioParqueoDetailContext(DbContextOptions<EspacioParqueoDetailContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<EspacioParqueoDetail> EspacioParqueoDetails { get; set; }
+    }
+}
---
+++ SEBASTIAN AGUDELO/UC-Parking-REST/Models/UsuarioDetailContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UC_Parking_REST.Models
+{
+    public class UsuarioDetailContext : DbContext
+    {
+
+        public UsuarioDetailContext(DbContextOptions<UsuarioDetailContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<UsuarioDetail> UsuarioDetails { get; set; }
+    }
+}
